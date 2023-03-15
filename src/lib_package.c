b2e2848d60baa5d0b0f8f9e0ae1193a63dc1e85b
Nicolas Lebedenco <nicolas@lebedenco.net>
2023-03-15 02:28:10 -0400

    Report embedded modules in stack trace with module name in square brackets, there is no need for a BC prefix.

--- src/lib_package.c
+++ src/lib_package.c
@@ -490,7 +490,7 @@ static int lj_cf_package_loader_preload(lua_State *L)
   lua_getfield(L, -1, name);
   if (lua_isnil(L, -1)) {  /* Not found? */
     const char *bcname = mksymname(L, name, SYMPREFIX_BC);
-    const char *chunkname = lua_pushfstring(L, "=[BC %s]", name);
+    const char *chunkname = lua_pushfstring(L, "=[%s]", name);
     const char *bcdata = ll_bcsym(NULL, bcname);
     if (bcdata == NULL || luaL_loadbuffer(L, bcdata, ~(size_t)0, chunkname) != 0)
       lua_pushfstring(L, "\n\tno field package.preload['%s']", name);
